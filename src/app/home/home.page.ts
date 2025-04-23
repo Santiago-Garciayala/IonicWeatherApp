b12368d3ccc56fbde1c1be730cb67423f00e398f
Santiago-Garciayala <g00434302@atu.ie>
2025-04-23 20:14:14 +0100

    got basic weather display working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,24 +3,39 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
 import { LocationService } from '../services/location.service';
 import { WeatherService } from '../services/weather.service';
 import { JsonPipe } from '@angular/common'; //used to display entire js objects with angular data binding, i used it for debugging
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonList, IonItem, IonLabel, IonAvatar } from '@ionic/angular/standalone';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'; //for using Swiper, https://ionicframework.com/docs/angular/slides
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, JsonPipe],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, JsonPipe, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonList, IonItem, IonLabel, IonAvatar],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], //for using Swiper
 })
 export class HomePage implements OnInit {
   title: string = "Ionic Weather App"
   location: any;
-  weatherObj: any;
+  weatherServPublic: any; //workaround to be able to use the service in the html since its normally private
+  currentWeather: any;
+  weatherForecast: any;
   constructor(private locationService: LocationService, private weatherService: WeatherService) { }
 
   async ngOnInit(): Promise<void> {
+    this.weatherServPublic = this.weatherService;
     this.location = await this.locationService.getCoordinates();
-    this.weatherService.getWeatherInfo(this.location).subscribe({
+    this.weatherService.getCurrentWeather(this.location).subscribe({
       next: (w) => {
-        this.weatherObj = w;
+        this.currentWeather = w;
+      },
+      error: (e) => {
+        //show error screen
+        console.error(e);
+      }
+    });
+    this.weatherService.getForecast3h5d(this.location).subscribe({
+      next: (w) => {
+        this.weatherForecast = w;
       },
       error: (e) => {
         //show error screen
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -11,10 +11,28 @@ export class WeatherService {
 
   constructor(private http: HttpClient) { }
 
-  getWeatherInfo(pos: Position): Observable<any> {
-
+  getCurrentWeather(pos: Position): Observable<any> {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${environment.OPEN_WEATHER_API_KEY}&units=metric`;
+    return this.http.get(url);
+  }
+
+  getForecast3h5d(pos: Position): Observable<any> {
+    let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${environment.OPEN_WEATHER_API_KEY}&units=metric`;
     console.log(url);
     return this.http.get(url);
+  }
+
+  getIconUrl(id: string, scale?: number): string {
+    let url: string = `https://openweathermap.org/img/wn/${id}@${scale}x.png`
+    return url;
+  }
+
+  formatDT(dt: string): string {
+    //use regex to format the date to the format HH:MM DD-MM from YYYY-MM-DD HH:MM:SS
+    let formatted: string = dt.replace(
+      /^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):\d{2}$/,
+      "$4:$5 $3-$2"
+    );
+    return formatted;
   }
 }
